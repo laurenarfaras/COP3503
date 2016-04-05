198f17579a3cc626e1064793873bbab090e7e669
laurenarfaras <larfaras96@gmail.com>
2016-04-05 19:48:40 -0400

    pushes FOR instead of begin; catches syntax errors; catches ONLY identifiers that start with a lowercase letter; catches some operators and delimiters found ONLY inside of an identifier string that starts with a lowercase letter

--- tres.cpp
+++ tres.cpp
@@ -22,6 +22,9 @@ int main(int argc, char **args) {
   //read from the file test1.txt
   std::ifstream ifs("test1.txt");
   int i = 0;
+  std::string syntaxErr[100];
+  int z = 0;
+  std::string identifiers[100];
 
   bool fur = false;
   int depth = 0;
@@ -33,30 +36,79 @@ int main(int argc, char **args) {
   std::string keyend = "END";
   bool dne = false;
 
+  bool equals = false;
+  bool semicolon = false;
+
   while (ifs) {
     std::string temp_in;
     ifs >> temp_in;
-    //std::cout << temp_in << std::endl;
-    if (temp_in.compare(keyfor) == 0) {
+    std::cout << temp_in << std::endl;
+    if (temp_in.compare("") == 0) {
+      break;
+    }
+    //check for the keyword FOR
+    else if (temp_in.compare(keyfor) == 0) {
       //std::cout << "I am in the FOR if statement" << std::endl;
       fur = true;
       rof = true;
+      s.push("FOR");
+      //compare the current depth with the max_depth and change as needed
+      depth = s.size();
+      if (depth > max_depth) {
+        max_depth = depth;
+      }
     }
+    //check for the keyword BEGIN
     else if (temp_in.compare(keybegin) == 0) {
       nigeb = true;
       if (fur) {
         //std::cout << "I should be increasing depth" << std::endl;
-        s.push("BEGIN");
-        depth = s.size();
-        if (depth > max_depth) {
-          max_depth = depth;
-        }
+        //s.push("BEGIN");
+        //depth = s.size();
+        //if (depth > max_depth) {
+        //  max_depth = depth;
+        //}
         fur = false;
       }
     }
+    //check for the keyword END
     else if (temp_in.compare(keyend) == 0) {
       dne = true;
       s.pop();
+    }
+    //check to see there is a syntax error
+    else if ((int)temp_in.at(0) >= 65 && (int)temp_in.at(0) <= 90) {
+      syntaxErr[i] = temp_in;
+      ++i;
+      //std::cout << "i=" << i << std::endl;
+    }
+    //check to see if an identifier
+    else if ((int)temp_in.at(0) >= 97 && (int)temp_in.at(0) <= 122) {
+      std::string fire;
+      for (int a = 0; a < temp_in.size(); a++) {
+        if ((int)temp_in.at(a) >= 97 && (int)temp_in.at(a) <= 122) {
+          fire = fire + temp_in.at(a);
+        } else if ((int)temp_in.at(a) == 61) {
+          equals = true;
+          if (fire.compare("") != 0) {
+            identifiers[z] = fire;
+            ++z;
+            fire = "";
+          }
+        } else if ((int)temp_in.at(a) == 59) {
+          semicolon = true;
+          if (fire.compare("") != 0) {
+            identifiers[z] = fire;
+            ++z;
+            fire = "";
+          }
+        }
+      }
+      if (fire.compare("") != 0) {
+        identifiers[z] = fire;
+        ++z;
+        fire = "";
+      }
     }
 
   }
@@ -73,6 +125,31 @@ int main(int argc, char **args) {
     std::cout << "END "<< std::endl;
   }
 
+  std::cout << "Syntax Error(s): ";
+  for (int j = 0; j < i; j++) {
+    std::cout << syntaxErr[j];
+  }
+  std::cout << "" << std::endl;
+
+  //std::cout << "identifire: " << identifiers[0] << std::endl;
+
+  std::cout << "Identifier(s): ";
+  for (int k = 0; k < z; k++) {
+    std::cout << identifiers[k] << " ";
+  }
+  std::cout << "" << std::endl;
+
+  std::cout << "Operator(s): ";
+  if (equals) {
+    std::cout << "= ";
+  }
+  std::cout << "" << std::endl;
+
+  std::cout << "Delimiter(s): ";
+  if (semicolon) {
+    std::cout << "; ";
+  }
+  std::cout << "" << std::endl;
 
 
 }
